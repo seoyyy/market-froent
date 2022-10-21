0227bc3d9953a0fd8c2fbf5a499922281cf04ade
seoyyy <seoyyy@gmail.com>
2022-10-21 08:56:48 +0000

    last try!

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     const history = useHistory();
     return (
         <div>
-            <h1>hi</h1>
+            <h1>hi~~</h1>
             <div id="header">
                 <HaderPage />
             </div>
